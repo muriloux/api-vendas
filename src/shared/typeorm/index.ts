88ef1ecd5e23f0d851b07a70f0e7bd0b0335becb
unknown <murilomelo.code@gmail.com>
2022-07-07 17:37:08 -0300

    inital commit

---
+++ src/shared/typeorm/index.ts
@@ -0,0 +1,11 @@
+import { createConnection } from 'typeorm';
+
+createConnection()
+    .then(() => {
+        console.log('Connected to database ✔');
+    })
+    .catch(error => {
+        console.log(
+            `Error trying to create connection with database: ${error}`,
+        );
+    });
