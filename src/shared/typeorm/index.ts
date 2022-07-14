b87f1e4268c7eef8467a854a4d3a688d0db94f9b
unknown <murilomelo.code@gmail.com>
2022-07-14 00:42:39 -0300

    major update

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,11 +1,9 @@
 import { createConnection } from 'typeorm';
 
 createConnection()
-    .then(() => {
-        console.log('Connected to database ✔');
-    })
-    .catch(error => {
-        console.log(
-            `Error trying to create connection with database: ${error}`,
-        );
-    });
+  .then(() => {
+    console.log('Connected to database ✔');
+  })
+  .catch(error => {
+    console.log(`Error trying to create connection with database: ${error}`);
+  });
