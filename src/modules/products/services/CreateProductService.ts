4335e587a27fe044c355769151a1de0f3cc63255
muriloux <murilomelo.code@gmail.com>
2022-07-29 17:51:39 -0300

    rename from UsersRepo to UserRepo, export it as default and update imports to new name

--- src/modules/products/services/CreateProductService.ts
+++ src/modules/products/services/CreateProductService.ts
@@ -1,7 +1,7 @@
 import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
 import Product from '../typeorm/entities/Product';
-import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
+import ProductRepository from '../typeorm/repositories/ProductRepository';
 
 interface IRequest {
   name: string;
@@ -10,16 +10,16 @@ interface IRequest {
 }
 
 class CreateProductService {
-  public async execute({ name, price, quantity }: IRequest): Promise<Product> {
+  public async execute({
+    name,
+    price,
+    quantity,
+  }: IRequest): Promise<Product | Error> {
     const productsRepository = getCustomRepository(ProductRepository);
     const productExists = await productsRepository.findByName(name);
 
-    try {
-      if (productExists) {
-        throw new AppError('There is already a product with this name.');
-      }
-    } catch (error) {
-      console.log(error);
+    if (productExists) {
+      throw new AppError('There is already a product with this name.');
     }
 
     const product = productsRepository.create({
@@ -29,7 +29,6 @@ class CreateProductService {
     });
 
     await productsRepository.save(product);
-
     return product;
   }
 }
