b23acae99ed75fa3cef250f31b8bce84ee3145f1
muriloux <murilomelo.code@gmail.com>
2022-07-29 17:56:47 -0300

    implement user creation and listing

---
+++ src/modules/users/services/CreateUserService.ts
@@ -0,0 +1,32 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import User from '../typeorm/entities/User';
+import UserRepository from '../typeorm/repositories/UserRepository';
+
+interface IRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  public async execute({ name, email, password }: IRequest): Promise<User> {
+    const userRepository = getCustomRepository(UserRepository);
+    const emailExists = await userRepository.findByEmail(email);
+
+    if (emailExists) {
+      throw new AppError('Email already registered.');
+    }
+
+    const user = userRepository.create({
+      name,
+      email,
+      password,
+    });
+    await userRepository.save(user);
+
+    return user;
+  }
+}
+
+export default CreateUserService;
---
+++ src/modules/users/typeorm/entities/User.ts
@@ -0,0 +1,40 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { v4 as uuidv4 } from 'uuid';
+
+@Entity('users')
+class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  avatar: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidv4();
+    }
+  }
+}
+
+export default User;
